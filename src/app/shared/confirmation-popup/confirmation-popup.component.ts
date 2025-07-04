303b7d52cd69da3083a4fbdc5bbde9600a73f8a0
marcosjrs <marcosjrs80@gmail.com>
2025-07-04 22:22:50 +0200

    feat: enhance confirmation prompts and localization for clearing tasks and data

--- src/app/shared/confirmation-popup/confirmation-popup.component.ts
+++ src/app/shared/confirmation-popup/confirmation-popup.component.ts
@@ -9,11 +9,11 @@ import { TranslocoModule } from '@ngneat/transloco';
   template: `
     <div class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center">
       <div class="relative p-5 border w-96 shadow-lg rounded-md bg-white">
-        <h3 class="text-xl font-bold mb-4">{{'Confirmation' | transloco}}</h3>
+        <h3 class="text-xl font-bold mb-4">{{'settings.confirmation' | transloco}}</h3>
         <p class="mb-4">{{ message | transloco }}</p>
         <div class="flex justify-end space-x-2">
-          <button (click)="confirm.emit(true)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">{{'Confirm' | transloco}}</button>
-          <button (click)="confirm.emit(false)" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">{{'Cancel' | transloco}}</button>
+          <button (click)="confirm.emit(true)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">{{'settings.confirm' | transloco}}</button>
+          <button (click)="confirm.emit(false)" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">{{'settings.cancel' | transloco}}</button>
         </div>
       </div>
     </div>
